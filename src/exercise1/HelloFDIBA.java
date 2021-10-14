60aba17a44c076d5e78967a75826829d3bff7318
VasilGrekov <>
2021-10-14 06:06:34 +0000

    Files updated

--- src/exercise1/HelloFDIBA.java
+++ src/exercise1/HelloFDIBA.java
@@ -1,9 +1,9 @@
 package exercise1;
 
-public class HelloFdiba {
+public class HelloFDIBA {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fdiba!");
+        System.out.println("Hello FDIBA!");
     }    
       
 }
